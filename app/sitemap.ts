3a6f8d2a81a78d7d1f0edba8c6933a61df5e7c74
Elsie <elsieoscar06@gmail.com>
2026-08-05 11:31:14 +0300

    added site map

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://tea-rific.vercel.app";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/menu`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
